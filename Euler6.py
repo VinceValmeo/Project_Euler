bbb4bb4a3029ade322ac267944d0e88446533113
Vnc <vivalmeo@gmail.com>
2022-06-01 12:34:34 +0800

    Example Solved

--- Euler6.py
+++ Euler6.py
@@ -1,1 +1,18 @@
-print("testing")
+import math
+
+counter = 1
+sumSquared = 0
+squaredSum = 0
+squaredSumAnswer = 0
+difference = 0
+while counter < 11:
+    sumSquared = sumSquared + math.pow(counter,2)
+    
+    squaredSum = squaredSum + counter
+    squaredSumAnswer = math.pow(int(squaredSum), 2)
+    counter = counter + 1
+    difference = squaredSumAnswer - sumSquared
+
+print("Sum of Squared: " + str(int(sumSquared)))
+print("Squared of Sum: " + str(int(squaredSumAnswer)))
+print("Difference: " + str(int(difference)))

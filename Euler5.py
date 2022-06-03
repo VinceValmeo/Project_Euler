ed4c6c2f21ff5a9643033ea2d73ac3c98ddb2753
Vnc <vivalmeo@gmail.com>
2022-06-03 23:06:07 +0800

    Instructions and all keyword testing

--- Euler5.py
+++ Euler5.py
@@ -1,9 +1,15 @@
-counter = 10
-quotient = 0
-limit = 2520
-while (counter != limit and counter != 0):
-    quotient = limit / counter
-    if(quotient % 2 == 0 ):
-        print(counter)
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# 
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-    counter = counter - 1
+arrays = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+number = 1
+isRunning = True
+print("Running...")
+while(isRunning):
+    if(number/all(array) % 2== 0 for array in arrays):
+        print(number)
+        number = number + 1
+    else:
+        number = number + 1
+    isRunning = False

d11cd2b2b9704e2ad001a6c07db3376e44247a16
Vnc <vivalmeo@gmail.com>
2022-06-08 21:21:27 +0800

    getting and printing list of sums

--- Euler12.py
+++ Euler12.py
@@ -1,9 +1,18 @@
 counter = 1
 sum = 0
+myList =[]
 while counter <= 7:
     print(counter)
     sum = counter + sum
+    # print("This is SUM: " + str(sum))
+    myList.append(sum)
     counter = counter + 1
  
-
+print("="*20)
+print("Total")
 print(sum)
+
+
+print("="*20)
+print("List of Sum")
+print(myList)

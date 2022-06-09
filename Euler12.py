3d7dfd906c39a40bb3f50b43f17d8a2b6b4bec20
Vnc <vivalmeo@gmail.com>
2022-06-09 22:36:37 +0800

    Testing getDivisible Function

--- Euler12.py
+++ Euler12.py
@@ -1,18 +1,31 @@
-counter = 1
-sum = 0
-myList =[]
-while counter <= 7:
-    print(counter)
-    sum = counter + sum
-    # print("This is SUM: " + str(sum))
-    myList.append(sum)
-    counter = counter + 1
+# counter = 1
+# sum = 0
+# myList =[]
+# while counter <= 7:
+#     print(counter)
+#     sum = counter + sum
+#     # print("This is SUM: " + str(sum))
+#     myList.append(sum)
+#     counter = counter + 1
  
-print("="*20)
-print("Total")
-print(sum)
+# print("="*20)
+# print("Total")
+# print(sum)
 
 
-print("="*20)
-print("List of Sum")
-print(myList)
+# print("="*20)
+# print("List of Sum")
+# print(myList)
+
+def getDivisible(number):
+    i = 1
+    factor = 0
+    while i != number:
+        if(number / i % 2 == 0):
+            factor = i
+            i = i + 1
+            return print(factor)
+        i = i + 1
+    i = i + 1
+number = 10
+getDivisible(number)

682c0490aaba9681af3c8930ff0bb909019e6489
Vnc <vivalmeo@gmail.com>
2022-07-13 06:30:33 +0800

    Attempt printing value in array

--- Euler20.py
+++ Euler20.py
@@ -8,4 +8,17 @@
 from math import factorial
 
 number = 100
-print(factorial(number))
+sum = 0
+factorialed = str(factorial(number))
+counter = 0
+arr = []
+# print("Total characters of the factorialed: "  + str(len(factorialed)))
+# while counter <= len(factorialed):
+#     sum = sum + int(factorialed[counter])
+#     counter = counter + 1
+
+for counter in str(len(factorialed)):
+    arr.append(factorialed)
+
+
+print(arr)

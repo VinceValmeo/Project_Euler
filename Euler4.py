2e6e95fb0baff1e838bf8dc6bc87d5649f132d56
Vnc <vivalmeo@gmail.com>
2022-07-20 06:08:02 +0800

    Edited: Euler4 - Palindrome Identifier Feature

--- Euler4.py
+++ Euler4.py
@@ -8,8 +8,21 @@ first_num = 0
 seconed_num = 0
 
 test_number = 9009
+arr_reverse = []
+arr = []
 
 counter = 0
 while counter < len(str(test_number)):
-    print(str(test_number)[counter])
+    # print(str(test_number)[counter])
+    arr.append(str(test_number)[counter])
+    arr_reverse.append(str(test_number)[counter])
+
     counter = counter + 1
+
+arr_reverse.reverse()
+print(arr_reverse)
+
+if arr_reverse == arr:
+    print("Palindrome")
+else:
+    print("NOT Palindrome")

ba26f273761a9dc2c1b29746ac4ceba292af4700
Vnc <vivalmeo@gmail.com>
2022-07-30 06:53:16 +0800

    Predefined Palindrome Checker

--- Euler4.py
+++ Euler4.py
@@ -3,41 +3,20 @@
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-
-# first_num = 10
-
-# while first_num > 0:
-#     print("*" * first_num)
-#     first_num = first_num - 1
-# test_number = 9009
-# arr_reverse = []
-# arr = []
-
-# counter = 0
-# while counter < len(str(test_number)):
-#     # print(str(test_number)[counter])
-#     arr.append(str(test_number)[counter])
-#     arr_reverse.append(str(test_number)[counter])
-
-#     counter = counter + 1
-
-# arr_reverse.reverse()
-# print(arr_reverse)
-
-# if arr_reverse == arr:
-#     print("Palindrome")
-# else:
-#     print("NOT Palindrome")
-
-myString = "Hello"
-
 def reverser(toReverse):
     reversedString = ""
     for counter in toReverse:
         reversedString = counter + reversedString
-    
     return reversedString
 
-print("Original String: " + myString)
+testString = "9009"
 
-print("Reversed String: " + reverser(myString))
+print("Original String: " + testString)
+print("Reversed String: " + reverser(testString))
+
+
+if testString == reverser(testString):
+    print("Is palindrome")
+else:
+    print("NOT a Palindrome")
+    

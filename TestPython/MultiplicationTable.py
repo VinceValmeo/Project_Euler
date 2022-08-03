e616e3d6cb4d5afbfbc57cc8637210294851d5fb
Vnc <vivalmeo@gmail.com>
2022-08-03 22:41:17 +0800

    Python Multiplication Table

---
+++ TestPython/MultiplicationTable.py
@@ -0,0 +1,29 @@
+print("Hello this is the Multiplication Table Program")
+
+
+first_counter = 1
+while (first_counter < 11):
+    second_counter = 1
+    while (second_counter < 11):
+
+
+        print(str(first_counter*second_counter) + "\t", end="")
+        second_counter += 1
+    print() 
+    first_counter += 1
+
+print("Program Ended")
+
+# print("Hello this is the Multiplication Table Program")
+
+
+# first_counter = 1
+# second_counter = 1
+# while (first_counter < 11):
+#     while (second_counter < 11):
+#         print(str(first_counter*second_counter) + "\t", end="")
+#         second_counter += 1
+#     print() 
+#     first_counter += 1
+
+# print("Program Ended")

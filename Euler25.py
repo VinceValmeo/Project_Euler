3f01ad346d9a1752230710036f49e18d685a1a72
Vnc <vivalmeo@gmail.com>
2022-08-09 22:15:53 +0800

    Fibonacci in array

--- Euler25.py
+++ Euler25.py
@@ -1,5 +1,3 @@
-
-
 # The Fibonacci sequence is defined by the recurrence relation:
 
 #     Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
@@ -22,3 +20,18 @@
 # The 12th term, F12, is the first term to contain three digits.
 
 # What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+f1 = 1
+f2 = 1
+temp = 0 
+
+num_array = [1 ,1 ]
+while f2 < 90:
+    temp = f1 + f2
+    f1 = f2
+    f2 = temp 
+    num_array.append(f2)
+    temp = 0
+
+print(num_array)
+print("Program Ended")
